31df32048916269744dabe44e1c423787e0bb9bc
haitaodesign <15393243613@163.com>
2017-04-11 21:33:34 +0800

    删除任务功能完成

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 	'use strict';
 	var $form_add_task=$('.add-task')
 		,task_list=[]
+		,$delete_task
 		;
 
 	init();
@@ -20,34 +21,64 @@
 		}
 	});
 
+	function listion_task_delete() {
+			$delete_task.on('click',function () {
+			var $this=$(this);
+			var $item = $this.parent().parent();
+			console.log($item);
+			var index = $item.data('index');
+			var tmp= confirm('确定删除？');
+			tmp ? delete_task(index) : null;
+		})
+	}
+
+	
+
 	function add_task(new_task) {
 		task_list.push(new_task);
 		store.set('task_list',task_list);
 		return true;
 	}
 
+	function refresh_task_list() {
+		store.set('task_list',task_list);
+		render_task_list(); 
+	}
+
+	function delete_task(index) {
+		if(index === undefined || !task_list[index]) return;
+		delete task_list[index];
+		console.log(task_list);
+		refresh_task_list();		
+	}
+
 	function init() {
 		task_list = store.get('task_list') || [];
 		if(task_list.length){
 			render_task_list();
+
 		}
+
 	}
 
 	function render_task_list() {
 		var $task_list=$('.task-list');
 		$task_list.html('');
 		for (var i = 0; i < task_list.length; i++) {
-			var $task = render_task_tpl(task_list[i]);
+			var $task = render_task_item(task_list[i],i);
 			$task_list.append($task);
 		}
+		$delete_task = $('.action.delete');
+		listion_task_delete();
 	}
 
-	function render_task_tpl(data) {
-		var list_item_tpl='<div class="task-item">'+
+	function render_task_item(data,index) {
+		if(!data || !index) return;
+		var list_item_tpl='<div class="task-item" data-index="'+index+'">'+
 			'<span><input type="checkbox"></span>'+
 			'<span class="task-content">'+data.content+'</span>'+
 			'<span class="fr">'+
-			'<span class="action"> 删除</span>'+
+			'<span class="action delete"> 删除</span>'+
 			'<span class="action"> 详请</span>'+
 			'</span>'+
 			'</div>';	
